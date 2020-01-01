e75029e5b36c4bd784e346a627a2609ca9b5e5ba
Jan Turon <janturon@email.cz>
2020-01-01 21:25:18 +0100

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # blitzbox
-Ten-liner vanilla javascript lightbox.
+
+Every lightbox bragging itself as "lightweight" or "simple" is just too bloated and complicated - at least what I found on internet. Use this vanilla javascript ten-liner like
+
+```
+<a href="big_image.jpg" class="blitzbox"><img src="thumbnail.jpg"></a>
+```
+
+Public domain. Enjoy.
---
+++ blitzbox.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var curtain = document.createElement("div");
+  curtain.style.cssText = "position: fixed; top: 0; left: 0; z-index: 1; width: 100%; height: 100%; background: rgba(0,0,0,0.5) none 50% 50%/contain no-repeat; display: none";
+  document.body.appendChild(curtain);
+  curtain.onclick = function() { curtain.style.display = "none"; }
+  var boxes = document.getElementsByClassName("blitzbox");
+  for(var i=0; i<boxes.length; ++i) boxes[i].addEventListener("click", function(e) {
+    e.preventDefault();
+    curtain.style.backgroundImage = "url('"+e.target.parentNode.href+"')";
+    curtain.style.display = "block";
+  });
+});
